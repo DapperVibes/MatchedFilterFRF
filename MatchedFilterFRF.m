b53d475c649d900272c4b13f2cc500978842e09e
Craig N Dolder <31927305+DapperVibes@users.noreply.github.com>
2017-09-15 16:39:47 +0100

    Daq now functioning with one channel
    Added DAQ functionality and now compares FRF against signal sent to output.

--- MatchedFilterFRF.m
+++ MatchedFilterFRF.m
@@ -1,38 +1,129 @@
-function [] = MatchedFilterFRF(DAQObject)
+function [FRF,fVecFRF] = MatchedFilterFRF(fs,f1,f2,TSweep,TTotal,DAQ)
 %MatchedFilterFRF - Controls DAQ hardware to send a matched filter sweep
 %and computes the Frequency Response Function of the return signal
 %
-% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+% Syntax:  [FRF,fVecFRF] = MatchedFilterFRF(fs,f1,f2,TSweep,TTotal,A,N)
 %
 % Inputs:
-%    input1 - Description
-%    input2 - Description
-%    input3 - Description
+%    fs     - Sampling frequency [Hz]
+%    f1     - Frequency to start sweep [Hz]
+%    f2     - Frequency to end sweep [Hz]
+%    TSweep - Duration of sweep [s]
+%    TTotal - Total time for aquisition from start of sweep [s]
+%    DAQ    - Handle for NI DAQ device with 1 input and 2 output channels
 %
 % Outputs:
-%    output1 - Description
-%    output2 - Description
+%    FRF    - Frequency response function [Ratio]
+%    fVecFRF- Frequency vector for frequency response function [Ratio]
 %
 % Example: 
-%    Line 1 of example
-%    Line 2 of example
-%    Line 3 of example
+% 
+% fs = 200000; % Sampling frequency [Hz]
+% 
+% f1 = 20000; % Lowest frequency of interest [Hz]
+% f2 = 80000; % Highest frequency of interest [Hz]
+% 
+% TSweep = 0.005; % Total time of sweep [s]
+% TTotal = 0.3;
+% dt = 1/fs; % Time step [s]
+% 
+% %% Initialize DAQ system 
+% 
+% % List NI DAQ devices
+% devices = daq.getDevices;
+% 
+% % Start NI DAQ session
+% s = daq.createSession('ni');
+% 
+% % Add input channel
+% chi0 = addAnalogInputChannel(s,'Dev3', 0, 'Voltage');
+% cho0 = addAnalogOutputChannel(s,'Dev3',0,'Voltage');
+% 
+% % Specify sampling frequency
+% s.Rate = fs;
+% 
+% %% Run FRF Code
+% 
+% [FRF,fVecFRF] =...
+%     MatchedFilterFRF(fs,f1,f2,TSweep,TTotal,s);
+% 
+% 
+% %% Figures
+% 
+% figure(5)
+% subplot(2,1,1)
+% semilogx(fVecFRF/1000,20*log10(abs(FRF)))
+% axis([f1/1000 f2/1000 -50 12])
+% title('FRF')
+% ylabel('Amplitude [dB]')
+% subplot(2,1,2)
+% semilogx(fVecFRF/1000,angle(FRF)*180/pi)
+% axis([f1/1000 f2/1000 -180 180])
+% ylabel('Phase [degrees]')
+% xlabel('Frequency [kHz]')
 %
 % Other m-files required: none
 % Subfunctions: none
 % MAT-files required: none
 %
-% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+% See also: none
 
 % Author: Craig Dolder
 % University of Southampton
 % email: C.N.Dolder@soton.ac.uk
-% Website: http://www.
-% Aug 2019; Last revision: 13-Aug-2017
+% Website: https://github.com/DapperVibes
+% Aug 2019; Last revision: 15-Aug-2017
+
+% Feature steps:
+% Complete:
+%  - File header
+%  - Calculate the desired logarithmic chirp
+%  - Add taper and buffer
+%  - Calculate the impulse response and return
+%  - Generate an artificial echo
+%  - Calculate the FRF and return
+%  - Send the chip via DAQ and calculate FRF of real return
+% To do:
+%  - Take two signals in and compute FRF between them
 
 %------------- BEGIN CODE --------------
 
-[data,time] = DAQObject.startForeground;
+% Time Vectors
+tVecSweep = 1/fs:1/fs:TSweep;
+tVecTotal = 1/fs:1/fs:TTotal;
+
+% Generate logarithmic chirp
+signal = chirp(tVecSweep,f1*0.9,TSweep-1/fs,f2*1.1,'logarithmic');
+
+% Exponential growth factor
+L = TSweep/log(f2/f1); % set exponential growth rate
+
+% Weighting of inverse signal
+invsignal = fliplr(f1.*exp(tVecSweep./L).*signal);
+
+% Buffer and taper sweep for equal energy
+signal = [signal.*tukeywin(length(signal),0.05).'...
+    zeros(1,length(tVecTotal) - length(tVecSweep))];
+
+%% Impulse response of signal when match filtered
+
+Ref = ifft(fft(signal(:),length(signal)).*fft(invsignal(:),length(signal)));
+
+%% DAQ
+
+queueOutputData(DAQ,signal(:));
+
+echo = DAQ.startForeground;
+
+%% Impulse response of echo when match filtered
+
+Response = ifft(fft(echo(:),length(signal)).*fft(invsignal(:),length(signal)));
+
+%% Calculate FRF
+
+FRF = fft(Response)./fft(Ref); FRF = FRF(1:end/2);
+
+fVecFRF = (0:(length(FRF)-1))*fs/(2*length(FRF));
 
 
 %------------- END OF CODE --------------
